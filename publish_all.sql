28755e3d3e712f4dcc243d5c847cf5c2d0f964ae
Abodid Sahoo <abodidsahoo@gmail.com>
2026-01-16 17:56:32 +0530

    used sqls removed

--- publish_all.sql
+++
@@ -1,7 +0,0 @@
--- PUBLISH EVERYTHING
--- Run this to make ALL your content visible immediately.
-
-UPDATE public.stories SET published = true;
-UPDATE public.posts SET published = true;
-UPDATE public.projects SET published = true;
-UPDATE public.films SET published = true;
--- upgrade_stories.sql
+++
@@ -1,4 +0,0 @@
--- UPGRADE STORIES
--- Add a 'content' column so we can use the Markdown Editor properly.
-
-ALTER TABLE public.stories ADD COLUMN IF NOT EXISTS content text;
