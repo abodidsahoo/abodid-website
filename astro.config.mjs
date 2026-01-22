acb7e43ce5e67d338d2ae97979b6bcc2540091b5
Abodid Sahoo <abodidsahoo@gmail.com>
2026-01-22 22:46:04 +0530

    sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import react from '@astrojs/react';
 
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   site: 'https://abodid.com', // Replace with your actual domain
 });
